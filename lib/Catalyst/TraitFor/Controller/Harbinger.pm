75ead205bff38dd3b7e830c8a44431d3e4042321
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-02-14 09:02:06 -0600

    assume Tee logger

--- lib/Catalyst/TraitFor/Controller/Harbinger.pm
+++ lib/Catalyst/TraitFor/Controller/Harbinger.pm
@@ -9,7 +9,9 @@ around auto => sub {
    my $req = $c->request;
    $env->{'harbinger.ident'} = $req->action;
    $env->{'harbinger.server'} = $c->config->{server};
-   $c->model('DB')->storage->debugobj($env->{'harbinger.querylog'});
+   $c->model('DB')->storage->debugobj->replace_logger(
+      harbinger => $env->{'harbinger.querylog'},
+   );
    $c->model('DB')->storage->debug(1);
 
    $self->$orig($c, @rest);
